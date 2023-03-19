f3023220118e3228e2d256d56adc1b49c372755f
Boxi Boxi <blago.dishkov@outlook.com>
2023-03-19 10:18:45 +0000

    new changes

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# Dishkovvwines
-wine website
+# Food and Wine Pairing APP
+
+
+# Description
+
+This project is simple App that helps you to pair your favourite food with best wine! 
+
+If you have favourite wine you can check what is best paring food with it! 
+
+# Visuals
+
+
+
+# Installation
+
+Simply click on the search bar and type your desired food or wine and you will get all the information need.
+
+No installation is required.
+
+Simply click on the web site.
+
+# Usage 
+
+React.js
+
+Node.js
+
+ReactQuery
+
+
+
+# Support
+
+If you have any questions please don't hesitate and contact blago.diskov@outlook.com.
+
+# Road Map
+
+Updates will be available very soon...!
+
+# Contributing
+
+if you want to make any changes or want to discuss please visit my github profile. [https://github.com/boxitooo6629]
+
+To use the API of https://spoonacular.com/food-api/docs you have to generate a personal API key. For full guide how to use the API please click here https://spoonacular.com/food-api/docs#Authentication. 
+
+Be mindful that there is daily quota points and you can quickly use the limit. If you want to mock some data you can copy the response an
+
+# Authors and Acknowledgment
+
+© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+
+# License
+
+MIT license
---
+++ src/App.jsx
@@ -0,0 +1,137 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import "./App.css";
+import wines from "./wines.json";
+
+const queryClient = new QueryClient();
+
+const baseURL = "https://api.spoonacular.com";
+const apiKey = process.env.REACT_APP_SPOONACULAR_API_KEY;
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Body />
+    </QueryClientProvider>
+  );
+}
+
+function Body() {
+  const [wine, setWine] = useState();
+  const [type, setType] = useState("wine");
+  const [wineForRecommendation, setWineForRecommendation] = useState();
+
+  const { data: pairings } = useQuery(
+    ["dishPairingForWine", baseURL, apiKey, wine],
+    async () => {
+      return fetch(
+        `${baseURL}/food/wine/dishes?wine=${wine}&apiKey=${apiKey}`
+      ).then((res) => res.json());
+    },
+    {
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      enabled: !!wine,
+    }
+  );
+
+  const { data: recommendations } = useQuery(
+    ["wineRecommendations", baseURL, apiKey, wine, wineForRecommendation],
+    async () => {
+      return fetch(
+        `${baseURL}/food/wine/recommendation?wine=${
+          wineForRecommendation || wine
+        }&number=2&apiKey=${apiKey}`
+      ).then((res) => res.json());
+    },
+    {
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      enabled: !!wineForRecommendation || !!wine,
+    }
+  );
+
+  const onFormSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      setWine(e.target.wine.value);
+      setWineForRecommendation(null)
+    },
+    [setWine]
+  );
+
+  useEffect(() => {
+    if (!wine) return;
+
+    let formated_wine = wine.toLowerCase().replaceAll(" ", "_");
+    setType(wines.wines.indexOf(formated_wine) !== -1 ? "wine" : "food");
+  }, [wine, setType]);
+
+  const onWineClick = useCallback((e) => {
+    e.preventDefault();
+    setWineForRecommendation(e.target.innerText);
+  }, []);
+
+  return (
+    <main>
+      <form className="search-form" onSubmit={onFormSubmit}>
+        <datalist id="wineSearchDataList">
+          {wines.wines.map((value, index) => (
+            <option key={index}>{value.replaceAll("_", " ")}</option>
+          ))}
+        </datalist>
+        <input
+          type="text"
+          list="wineSearchDataList"
+          className="search-input"
+          placeholder="Food or wine"
+          name="wine"
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <section className="results-container">
+        {!!pairings?.pairings?.length && (
+          <aside className="pairings-container">
+            <h2>Pairs best with</h2>
+            {type === "food" && (
+              <ul>
+                {pairings?.pairings?.map((value, index) => (
+                  <li key={index}>
+                    <button onClick={onWineClick}>{value}</button>
+                  </li>
+                ))}
+              </ul>
+            )}
+            {type === "wine" && (
+              <ul>
+                {pairings?.pairings?.map((value, index) => (
+                  <li key={index}>{value}</li>
+                ))}
+              </ul>
+            )}
+          </aside>
+        )}
+        {!!recommendations?.recommendedWines?.length && (
+          <div className="recommendations-container">
+            <h2>Wine Recommendations</h2>
+            <ul>
+              {recommendations?.recommendedWines?.map((wine, index) => (
+                <li key={index}>
+                  <h3>{wine.title}</h3>
+                  <img src={wine.imageUrl} alt={wine.title} />
+                  <span className="wine-recommendations-price">
+                    {wine.price}
+                  </span>
+                  <p>{wine.description}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </section>
+    </main>
+  );
+}
+export default App;
