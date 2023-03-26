7329b525579c0d4d5aff13814ca3236eb4672ab6
Bronwen27 <118174027+Bronwen27@users.noreply.github.com>
2023-03-26 02:25:37 +0100

    changed styling of profiles such as creating transition to text on hover of profile picture

--- src/Modules/TeamPage.jsx
+++ src/Modules/TeamPage.jsx
@@ -9,6 +9,17 @@ import github from "../assets/images/github-logo.png";
 import linkedin from "../assets/images/linkedin-logo.png";
 
 const TeamPage = () => {
+
+  // create hover function changing profile picture to info text
+  const handleHover = (event) => {
+    event.target.style.opacity = 0;
+  };
+
+  const handleMouseOut = (event) => {
+    event.target.style.opacity = 1;
+  };
+
+
   return (
     <Container
       maxWidth="1"
@@ -34,19 +45,24 @@ const TeamPage = () => {
               flexDirection: "column",
             }}
           >
+            <div class="logo-holder">
             <img
+              id="blagoPicture"
               className="profilePictures"
               src={blago}
               alt=""
+              onMouseOver={handleHover}
+              onMouseOut={handleMouseOut}
             />
             <h2>Blago Dishkov</h2>
-            <h3>
-              Proffesion: Viticulturist
+            <h3 className="textBlago">
+              <br></br><br></br>Proffesion: Viticulturist
 
-             <br></br> Hobby: Wine, Football, Fishing
+             <br></br><br></br> Hobby: Wine, Football, Fishing
 
-              <br></br>Lately intrested in Software Engineering and Web Development
+              <br></br><br></br>Lately intrested in Software Engineering and Web Development
             </h3>
+            </div>
           </Box>
         </Grid>
         <Grid xl={4}>
@@ -59,13 +75,17 @@ const TeamPage = () => {
               flexDirection: "column",
             }}
           >
+              <div class="logo-holder">
             <img
+              id="gellertPicture"
               className="profilePictures"
               src={gellert}
               alt=""
+           
             />
             <h2>Gellert Eke</h2>
-            <h3>
+            <h3 className="textGellert">
+              <br></br><br></br>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -74,6 +94,7 @@ const TeamPage = () => {
               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
               sunt in culpa qui officia deserunt mollit anim id est laborum.
             </h3>
+            </div>
           </Box>
         </Grid>
         <Grid xl={4}>
@@ -86,20 +107,26 @@ const TeamPage = () => {
               flexDirection: "column",
             }}
           >
+             <div class="logo-holder">
             <img
+            id="bronwenPicture"
               className="profilePictures"
               src={bronwen}
               alt=""
+    
             />
              <h2>Bronwen Jenner</h2>
-            <h3>
+            <h3 className="textBronwen">
+              <br></br><br></br>
               Front-End Web Development Student 
+              <br></br>
               <br></br>Studying with EdX Bootcamp
 
-             <br></br>  <img src={github} alt="GitHub" /> Github: <a href="https://github.com/Bronwen27">Bronwen27</a>
+             <br></br><br></br>  <img src={github} alt="GitHub" /> Github: <a href="https://github.com/Bronwen27">Bronwen27</a>
 
-              <br></br>  <img src={linkedin} alt="GitHub" /> LinkedIn: <a href="https://www.linkedin.com/in/bronwen-jenner-a376a5195/ ">Bronwen-Jenner</a>
+              <br></br><br></br>  <img src={linkedin} alt="GitHub" /> LinkedIn: <a href="https://www.linkedin.com/in/bronwen-jenner-a376a5195/ ">Bronwen-Jenner</a>
             </h3>
+            </div>
           </Box>
         </Grid>
       </Grid>
